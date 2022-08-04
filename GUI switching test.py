871222911f895b654d767396379024726683a39f
gabolinscya <gabolinscya@stu.otc.school.nz>
2022-08-05 09:38:39 +1200

    Figuring out how to make text not affect the other widgets on the same column

--- GUI switching test.py
+++ GUI switching test.py
@@ -1,38 +1,143 @@
-from tkinter import *
-from tkinter import ttk
-
-root = Tk()
-root.title("Andrew's L2 Assessment Program")
-
-opening_frame = ttk.LabelFrame(root, text="Title Screen")
-opening_frame.grid(row=0, column=0, columnspan=10, padx=10, pady=10, sticky="NSEW")
+try:
+    import tkinter as tk  # python 3
+    from tkinter import font as tkfont, ttk  # python 3
+except ImportError:
+    import Tkinter as tk  # python 2
+    import tkFont as tkfont  # python 2
 
 
-def screen_1():
-    clear_frame()
-    title1_text = StringVar()
-    title1_text.set("Video Game")
-    title1_label = ttk.Label(opening_frame, textvariable=title1_text)
-    title1_label.grid(row=0, column=0, columnspan=10, padx=10, pady=10)
-    change_button = ttk.Button(opening_frame, text="Change", command=screen_2())
-    change_button.grid(row=0, column=1)
+class SampleApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title_font = tkfont.Font(family='Times New Roman Baltic', size=120, weight="bold")
+        self.menu_button_font = tkfont.Font(family='Helvetica', size=18, weight="bold")
+        self.geometry('1280x720')
+        container = tk.Frame(self)
+        container.grid()
+        container.grid_rowconfigure(1, weight=1)
+        container.grid_columnconfigure(1, weight=1)
+
+        self.frames = {}
+        for F in (OpeningPage, MainMenu, DungeonDelve, CreateTeamPage, CreditPage, How2PlayPage, LeaderboardPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0)
+
+        self.show_frame("OpeningPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        for frame in self.frames.values():
+            frame.grid_remove()
+        frame = self.frames[page_name]
+        frame.tkraise()
+        frame.grid()
+
+    def breakcode(self):
+        self.destroy()
 
 
-def screen_2():
-    clear_frame()
-    title2_text = StringVar()
-    title2_text.set("Video Game2")
-    title2_label = ttk.Label(opening_frame, textvariable=title2_text)
-    title2_label.grid(row=0, column=0, columnspan=10, padx=10, pady=10)
-    change1_button = ttk.Button(opening_frame, text="Change", command=screen_1())
-    change1_button.grid(row=0, column=1)
+class OpeningPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Game Title", font=controller.title_font)
+        label.grid(row=1, column=5, pady=10)
+        self.grid_columnconfigure(4, weight=1)
+        button = tk.Button(self, text="Start Game", padx=100, pady=80, font=controller.menu_button_font,
+                           command=lambda: controller.show_frame("MainMenu"))
+        button.grid(row=6, column=5)
 
 
-def clear_frame():
-    for widgets in opening_frame.winfo_children():
-        widgets.destroy()
+class MainMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Surface Menu", font=controller.title_font)
+        label.grid(column=1)
+        buttonDungeon = tk.Button(self, text="Delve into the Dungeon",
+                                  command=lambda: controller.show_frame("DungeonDelve"))
+        buttonTeam = tk.Button(self, text="Champion Camp",
+                               command=lambda: controller.show_frame("CreateTeamPage"))
+        buttonCredit = tk.Button(self, text="Credits",
+                                 command=lambda: controller.show_frame("CreditPage"))
+        buttonH2P = tk.Button(self, text="How to Play",
+                              command=lambda: controller.show_frame("How2PlayPage"))
+        buttonLeaderboard = tk.Button(self, text="Leaderboard",
+                                      command=lambda: controller.show_frame("LeaderboardPage"))
+        buttonQuit = tk.Button(self, text="Exit game",
+                               command=lambda: controller.breakcode())
+        buttonDungeon.grid(row=1, column=0)
 
 
-opening_button = ttk.Button(opening_frame, text="Open", command=screen_1())
-opening_button.grid(row=0, column=1, columnspan=10, padx=10, pady=10)
-root.mainloop()
+        buttonTeam.grid(row=2, column=1, sticky="ew")
+        buttonCredit.grid(row=3, column=2)
+        buttonH2P.grid(row=4, column=3)
+        buttonLeaderboard.grid(row=5, column=4)
+        buttonQuit.grid(row=6, column=5)
+
+
+class DungeonDelve(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Dungeon", font=controller.title_font)
+        label.grid(row=0, pady=10)
+
+        buttonReturn = tk.Button(self, text="Return to Menu",
+                           command=lambda: controller.show_frame("MainMenu"))
+        buttonReturn.grid()
+
+
+class CreateTeamPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Champion Camp", font=controller.title_font)
+        label.grid(row=0, sticky="nsew", pady=10)
+        button = tk.Button(self, text="Return to Menu",
+                           command=lambda: controller.show_frame("MainMenu"))
+        button.grid()
+
+
+class CreditPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Credits", font=controller.title_font)
+        label.grid(row=0, sticky="nsew", pady=10)
+        button = tk.Button(self, text="Return to Menu",
+                           command=lambda: controller.show_frame("MainMenu"))
+        button.grid()
+
+
+class How2PlayPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="How To Play", font=controller.title_font)
+        label.grid(row=0, sticky="nsew", pady=10)
+        button = tk.Button(self, text="Return to Menu",
+                           command=lambda: controller.show_frame("MainMenu"))
+        button.grid()
+
+
+class LeaderboardPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Leaderboards", font=controller.title_font)
+        label.grid(row=0, sticky="nsew", pady=10)
+        button = tk.Button(self, text="Return to Menu",
+                           command=lambda: controller.show_frame("MainMenu"))
+        button.grid()
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
