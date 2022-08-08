d316116b7cf66bd817eb007e10199cfd0c3d3bfb
gabolinscya <gabolinscya@stu.otc.school.nz>
2022-08-08 13:27:47 +1200

    Worked with moving around text widgets to look presentable. Started work on a login screen to set up users accounts, currently only have the login screen semi-done. Still need to work on registering account details and saving that info. Because of this login account system i've added a logout button on the main menu. i've also swapped the leaderboard and credits button so that the credits button is further done the menu and the leaderboard is further up.

--- GUI switching test.py
+++ GUI switching test.py
@@ -13,12 +13,12 @@ class SampleApp(tk.Tk):
         self.menu_button_font = tkfont.Font(family='Helvetica', size=18, weight="bold")
         self.geometry('1280x720')
         container = tk.Frame(self)
-        container.grid()
-        container.grid_rowconfigure(1, weight=1)
-        container.grid_columnconfigure(1, weight=1)
+        container.grid(sticky="nsew")
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (OpeningPage, MainMenu, DungeonDelve, CreateTeamPage, CreditPage, How2PlayPage, LeaderboardPage):
+        for F in (OpeningPage, MainMenu, DungeonDelve, CreateTeamPage, CreditPage, How2PlayPage, LeaderboardPage, LoginMenu, RegisterMenu):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -47,11 +47,59 @@ class OpeningPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="Game Title", font=controller.title_font)
-        label.grid(row=1, column=5, pady=10)
-        self.grid_columnconfigure(4, weight=1)
+        label.grid(row=1, column=2, pady=10, sticky="new")
+        invis_label1 = tk.Label(self)
+        invis_label2 = tk.Label(self)
+        invis_label3 = tk.Label(self)
+
         button = tk.Button(self, text="Start Game", padx=100, pady=80, font=controller.menu_button_font,
+                           command=lambda: controller.show_frame("LoginMenu"))
+        invis_label1.grid(column=2, row=2, pady=50)
+        invis_label2.grid(column=1, row=1, padx=95)
+        invis_label3.grid(column=1, row=2, padx=95)
+        button.grid(row=3, column=2)
+
+class LoginMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Game Title", font=controller.title_font)
+        label.grid(row=1, column=2, pady=10, sticky="new")
+        invis_label1 = tk.Label(self)
+        invis_label2 = tk.Label(self)
+        invis_label3 = tk.Label(self)
+        Loginbutton = tk.Button(self, text="Login", font=controller.menu_button_font,
                            command=lambda: controller.show_frame("MainMenu"))
-        button.grid(row=6, column=5)
+        Regibutton = tk.Button(self, text="Register", font=controller.menu_button_font,
+                           command=lambda: controller.show_frame("RegisterMenu"))
+        Returnbutton = tk.Button(self, text="Back", font=controller.menu_button_font,
+                           command=lambda: controller.show_frame("OpeningPage"))
+        username_label = ttk.Label(self, text="Username:")
+        username_entry = ttk.Entry(self)
+        password_label = ttk.Label(self, text="Password:")
+        password_entry = ttk.Entry(self)
+        invis_label1.grid(column=2, row=2, pady=50)
+        invis_label2.grid(column=1, row=1, padx=95)
+        invis_label3.grid(column=1, row=2, padx=95)
+        Loginbutton.grid(row=5, column=2)
+        Regibutton.grid(row=6, column=2)
+        Returnbutton.grid(row=7, column=2)
+        username_label.grid(row=3, column=2, ipadx=100, padx=10, pady=10)
+        username_entry.grid(row=3, column=2, padx=10, pady=10)
+        password_label.grid(row=4, column=2, ipadx=100, padx=10, pady=10)
+        password_entry.grid(row=4, column=2, padx=10, pady=10)
+
+
+class RegisterMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Register your account", font=controller.title_font)
+        label.grid(row=0, pady=10)
+
+        confirm_registerbutton = tk.Button(self, text="Register Details",
+                           command=lambda: controller.show_frame("LoginMenu"))
+        confirm_registerbutton.grid()
 
 
 class MainMenu(tk.Frame):
@@ -59,27 +107,42 @@ class MainMenu(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="Surface Menu", font=controller.title_font)
-        label.grid(column=1)
-        buttonDungeon = tk.Button(self, text="Delve into the Dungeon",
+        label.grid(column=2)
+        invis_label1 = tk.Label(self)
+        invis_label2 = tk.Label(self)
+        invis_label3 = tk.Label(self)
+        invis_label4 = tk.Label(self)
+        invis_label5 = tk.Label(self)
+        invis_label6 = tk.Label(self)
+        invis_label7 = tk.Label(self)
+        buttonDungeon = tk.Button(self, text="Delve into the Dungeon", padx=10, pady=10, font=controller.menu_button_font,
                                   command=lambda: controller.show_frame("DungeonDelve"))
-        buttonTeam = tk.Button(self, text="Champion Camp",
+        buttonTeam = tk.Button(self, text="Champion Camp", padx=10, pady=10, font=controller.menu_button_font,
                                command=lambda: controller.show_frame("CreateTeamPage"))
-        buttonCredit = tk.Button(self, text="Credits",
+        buttonCredit = tk.Button(self, text="Credits", padx=10, pady=10, font=controller.menu_button_font,
                                  command=lambda: controller.show_frame("CreditPage"))
-        buttonH2P = tk.Button(self, text="How to Play",
+        buttonH2P = tk.Button(self, text="How to Play", padx=10, pady=10, font=controller.menu_button_font,
                               command=lambda: controller.show_frame("How2PlayPage"))
-        buttonLeaderboard = tk.Button(self, text="Leaderboard",
+        buttonLeaderboard = tk.Button(self, text="Leaderboard", padx=10, pady=10, font=controller.menu_button_font,
                                       command=lambda: controller.show_frame("LeaderboardPage"))
-        buttonQuit = tk.Button(self, text="Exit game",
+        buttonLogout= tk.Button(self, text="Log Out", padx=10, pady=10, font=controller.menu_button_font,
+                                command=lambda: controller.show_frame("LoginMenu"))
+        buttonQuit = tk.Button(self, text="Exit game", padx=10, pady=10, font=controller.menu_button_font,
                                command=lambda: controller.breakcode())
-        buttonDungeon.grid(row=1, column=0)
-
-
-        buttonTeam.grid(row=2, column=1, sticky="ew")
-        buttonCredit.grid(row=3, column=2)
-        buttonH2P.grid(row=4, column=3)
-        buttonLeaderboard.grid(row=5, column=4)
-        buttonQuit.grid(row=6, column=5)
+        buttonDungeon.grid(row=1, column=2, pady=2, sticky="w")
+        buttonTeam.grid(row=2, column=2, pady=2, sticky="w")
+        buttonCredit.grid(row=5, column=2, pady=2, sticky="w")
+        buttonH2P.grid(row=4, column=2, pady=2, sticky="w")
+        buttonLeaderboard.grid(row=3, column=2, pady=2, sticky="w")
+        buttonLogout.grid(row=6,column=2, pady=2, sticky="w")
+        buttonQuit.grid(row=7, column=2, pady=2, sticky="w")
+        invis_label1.grid(column=1, row=1, padx=50)
+        invis_label2.grid(column=1, row=2, padx=50)
+        invis_label3.grid(column=1, row=3, padx=50)
+        invis_label4.grid(column=1, row=4, padx=50)
+        invis_label5.grid(column=1, row=5, padx=50)
+        invis_label6.grid(column=1, row=6, padx=50)
+        invis_label7.grid(column=1, row=7, padx=50)
 
 
 class DungeonDelve(tk.Frame):
