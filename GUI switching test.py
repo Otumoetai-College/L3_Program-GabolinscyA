5212d64b876ee44b38e639c3d0804146d827ad4e
gabolinscya <gabolinscya@stu.otc.school.nz>
2022-08-10 15:17:48 +1200

    Added code that puts the users account details into a text document to be used later for logins. Currently have problems with getting an addition window to open and having buttons work properally.

--- GUI switching test.py
+++ GUI switching test.py
@@ -19,7 +19,10 @@ class SampleApp(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (OpeningPage, MainMenu, DungeonDelve, CreateTeamPage, CreditPage, How2PlayPage, LeaderboardPage, LoginMenu, RegisterMenu):
+        for F in (
+                OpeningPage, MainMenu, DungeonDelve, CreateTeamPage, CreditPage, How2PlayPage, LeaderboardPage,
+                LoginMenu,
+                RegisterMenu):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -41,6 +44,92 @@ class SampleApp(tk.Tk):
 
     def breakcode(self):
         self.destroy()
+
+    def check_password(self, username_entry, password_entry, confirm_password_entry):
+        global no_pw_warning
+        global not_equal_pw_warning
+        global no_us_and_pw_warning
+        global no_us_warning
+        inputted_username = username_entry.get()
+        inputted_username.strip()
+        inputted_password = password_entry.get()
+        inputted_password.strip()
+        inputted_confirm_password = confirm_password_entry.get()
+        inputted_confirm_password.strip()
+        if inputted_password == "":
+            if inputted_username == "":
+                not_equal_pw_warning = 0
+                no_pw_warning = 0
+                no_us_warning = 0
+                no_us_and_pw_warning = 0
+                no_us_and_pw_warning = ttk.Label(self, text="Username and Password cannot be empty")
+                no_us_and_pw_warning.grid(row=3, column=0, padx=10, pady=10)
+            else:
+                not_equal_pw_warning = 0
+                no_us_warning = 0
+                no_us_and_pw_warning = 0
+                no_pw_warning = 0
+                no_pw_warning = ttk.Label(self, text="Password cannot be empty")
+                no_pw_warning.grid(row=3, column=0, padx=10, pady=10)
+        elif inputted_confirm_password == inputted_password:
+            if inputted_username == "":
+                not_equal_pw_warning = 0
+                no_pw_warning = 0
+                no_us_and_pw_warning = 0
+                no_us_warning = 0
+                no_us_warning = ttk.Label(self, text="Username cannot be empty")
+                no_us_warning.grid(row=3, column=0, padx=10, pady=10)
+            else:
+                no_pw_warning = 0
+                not_equal_pw_warning = 0
+                no_us_and_pw_warning = 0
+                no_us_warning = 0
+                self.final_register_check(inputted_username, inputted_password)
+        else:
+            no_pw_warning = 0
+            no_us_and_pw_warning = 0
+            no_us_warning = 0
+            not_equal_pw_warning = 0
+            not_equal_pw_warning = ttk.Label(self, text="Passwords aren't the same")
+            not_equal_pw_warning.grid(row=3, column=0, padx=10, pady=10)
+
+    def final_register_check(self, inputted_username, inputted_password):
+        register_window = tk.Toplevel()
+        register_window.title("Confirmation")
+        register_window_label = ttk.Label(register_window, text="Are you sure you want to register with these details?")
+        register_window_button_yes = ttk.Button(register_window, text="Yes",
+                                                command=self.user_account_set(inputted_username, inputted_password,
+                                                                              register_window))
+        register_window_button_no = ttk.Button(register_window, text="No",
+                                               command=lambda: self.destroy_register_window(register_window))
+        register_window_label.grid(column=1, row=1)
+        register_window_button_yes.grid(column=1, row=2)
+        register_window_button_no.grid(column=1, row=3)
+
+    def destroy_register_window(self, register_window):
+        register_window.destroy()
+
+    def user_account_set(self, inputted_username, inputted_password, register_window):
+        global successful_user_addition
+        file = open("C:/Users/gabolinscya/Documents/L2_ASSIGNMENT_RPG/account_data_username.txt", "a")
+        file.write("\n")
+        file.write(inputted_username)
+        file.close()
+        file = open("C:/Users/gabolinscya/Documents/L2_ASSIGNMENT_RPG/account_data_password.txt", "a")
+        file.write("\n")
+        file.write(inputted_password)
+        file.close()
+        register_window.destroy()
+        successful_user_addition = ttk.Label(self, text="You're account has been created!")
+        successful_user_addition.grid(row=3, column=0, padx=10, pady=10)
+
+    def register_to_login(self):
+        no_pw_warning = 0
+        no_us_and_pw_warning = 0
+        no_us_warning = 0
+        not_equal_pw_warning = 0
+        successful_user_addition = 0
+        self.show_frame("LoginMenu")
 
 
 class OpeningPage(tk.Frame):
@@ -71,15 +160,15 @@ class LoginMenu(tk.Frame):
         invis_label2 = tk.Label(self)
         invis_label3 = tk.Label(self)
         Loginbutton = tk.Button(self, text="Login", font=controller.menu_button_font,
-                           command=lambda: controller.show_frame("MainMenu"))
+                                command=lambda: controller.show_frame("MainMenu"))
         Regibutton = tk.Button(self, text="Register", font=controller.menu_button_font,
-                           command=lambda: controller.show_frame("RegisterMenu"))
+                               command=lambda: controller.show_frame("RegisterMenu"))
         Returnbutton = tk.Button(self, text="Back", font=controller.menu_button_font,
-                           command=lambda: controller.show_frame("OpeningPage"))
+                                 command=lambda: controller.show_frame("OpeningPage"))
         username_label = ttk.Label(self, text="Username:")
         username_entry = ttk.Entry(self)
         password_label = ttk.Label(self, text="Password:")
-        password_entry = ttk.Entry(self)
+        password_entry = ttk.Entry(self, show="*")
         invis_label1.grid(column=2, row=2, pady=50)
         invis_label2.grid(column=1, row=1, padx=95)
         invis_label3.grid(column=1, row=2, padx=95)
@@ -104,12 +193,15 @@ class RegisterMenu(tk.Frame):
         username_label = ttk.Label(self, text="Username:")
         username_entry = ttk.Entry(self)
         password_label = ttk.Label(self, text="Password:")
-        password_entry = ttk.Entry(self)
+        password_entry = ttk.Entry(self, show="*")
         confirm_password_label = ttk.Label(self, text="Confirm Password:")
-        confirm_password_entry = ttk.Entry(self)
+        confirm_password_entry = ttk.Entry(self, show="*")
         confirm_registerbutton = tk.Button(self, text="Register Details",
-                           command=lambda: controller.show_frame("LoginMenu"))
-
+                                           command=lambda: controller.check_password(username_entry, password_entry,
+                                                                                     confirm_password_entry))
+        back_to_login = tk.Button(self, text="Back to Login",
+                                  command=lambda: controller.register_to_login())
+        back_to_login.grid(row=7, column=1, padx=10, pady=10)
         confirm_registerbutton.grid(row=6, column=1)
         username_label.grid(row=3, column=1, ipadx=100, padx=10, pady=10)
         username_entry.grid(row=3, column=1, padx=10, pady=10)
@@ -120,25 +212,6 @@ class RegisterMenu(tk.Frame):
         invis_label1.grid(row=0, pady=15)
         invis_label2.grid(column=0, row=1, padx=25)
         invis_label3.grid(row=2, column=1, pady=35)
-
-    def check_password(self, password_entry, confirm_password_entry):
-        password_entry.strip()
-        if password_entry == "":
-            no_pw_warning = ttk.Label(self, text="Password cannot be empty")
-            no_pw_warning.grid(row=4, column=1, ipadx=-100, fg="red", weight="bold")
-        if confirm_password_entry == password_entry:
-            no_pw_warning.destroy()
-            final_register_check(self)
-
-    def final_register_check(self):
-        register_window = Toplevel()
-        register_window.title("Confirmation")
-        register_window_label = ttk.Label(register_window, text="Are you sure you want to register with these details?")
-        register_window_button_yes = ttk.Button(register_window, text="Yes", command=)
-        register_window_button_no = ttk.Button(register_window, text="No", command=register_window.destroy())
-        register_window_label.grid(column=1, row=1)
-        register_window_button_yes.grid(column=1, row=2)
-        register_window_button_no.grid(column=1, row=3)
 
 
 class MainMenu(tk.Frame):
@@ -154,7 +227,8 @@ class MainMenu(tk.Frame):
         invis_label5 = tk.Label(self)
         invis_label6 = tk.Label(self)
         invis_label7 = tk.Label(self)
-        buttonDungeon = tk.Button(self, text="Delve into the Dungeon", padx=10, pady=10, font=controller.menu_button_font,
+        buttonDungeon = tk.Button(self, text="Delve into the Dungeon", padx=10, pady=10,
+                                  font=controller.menu_button_font,
                                   command=lambda: controller.show_frame("DungeonDelve"))
         buttonTeam = tk.Button(self, text="Champion Camp", padx=10, pady=10, font=controller.menu_button_font,
                                command=lambda: controller.show_frame("CreateTeamPage"))
@@ -164,8 +238,8 @@ class MainMenu(tk.Frame):
                               command=lambda: controller.show_frame("How2PlayPage"))
         buttonLeaderboard = tk.Button(self, text="Leaderboard", padx=10, pady=10, font=controller.menu_button_font,
                                       command=lambda: controller.show_frame("LeaderboardPage"))
-        buttonLogout= tk.Button(self, text="Log Out", padx=10, pady=10, font=controller.menu_button_font,
-                                command=lambda: controller.show_frame("LoginMenu"))
+        buttonLogout = tk.Button(self, text="Log Out", padx=10, pady=10, font=controller.menu_button_font,
+                                 command=lambda: controller.show_frame("LoginMenu"))
         buttonQuit = tk.Button(self, text="Exit game", padx=10, pady=10, font=controller.menu_button_font,
                                command=lambda: controller.breakcode())
         buttonDungeon.grid(row=1, column=2, pady=2, sticky="w")
@@ -173,7 +247,7 @@ class MainMenu(tk.Frame):
         buttonCredit.grid(row=5, column=2, pady=2, sticky="w")
         buttonH2P.grid(row=4, column=2, pady=2, sticky="w")
         buttonLeaderboard.grid(row=3, column=2, pady=2, sticky="w")
-        buttonLogout.grid(row=6,column=2, pady=2, sticky="w")
+        buttonLogout.grid(row=6, column=2, pady=2, sticky="w")
         buttonQuit.grid(row=7, column=2, pady=2, sticky="w")
         invis_label1.grid(column=1, row=1, padx=50)
         invis_label2.grid(column=1, row=2, padx=50)
@@ -192,7 +266,7 @@ class DungeonDelve(tk.Frame):
         label.grid(row=0, pady=10)
 
         buttonReturn = tk.Button(self, text="Return to Menu",
-                           command=lambda: controller.show_frame("MainMenu"))
+                                 command=lambda: controller.show_frame("MainMenu"))
         buttonReturn.grid()
 
 
